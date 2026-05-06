0f1ed42534dfb54f8c12916e76e1d28d327af97d
Teesma M <teeshma@icloud.com>
2026-05-05 20:04:56 -0700

    Add GitHub Pages files (index + screenshots

---
+++ GlowMoon/Extensions/DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Teesma M on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
